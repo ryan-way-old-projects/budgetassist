875e60b3ae3d9d3a29cc72b2e490153badd981d9
ryan way <ryan.keith.way@gmail.com>
2018-09-22 16:08:54 -0700

    added definition for transaction class

---
+++ transaction.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from datetime import date
+
+class UnsortedTransaction:
+    date = date.min
+    amount = 0
+    location = ""
+
+
+    def __init__(self, date, amount, location):
+        self.date = date
+        self.amount = amount
+        self.location = location
+
+def main():
+    date = datetime.strptime("09/20/2018", "%m/%d/%Y").date()
+
+    transaction = UnsortedTransaction(
+        date,
+        -7.34,
+        "Alberto's"
+    )
+
+    print(transaction.date)
+    print(transaction.amount)
+    print(transaction.location)
+
+if __name__ == "__main__":
+    main()
