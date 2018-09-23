366312e413995f22848637722e3b741e80cccd52
ryan way <ryan.keith.way@gmail.com>
2018-09-22 20:50:08 -0700

    added unittests for transaction class

---
+++ tests/transaction_test.py
@@ -0,0 +1,31 @@
+from transaction import UnsortedTransaction
+from datetime import datetime
+import unittest
+
+class UnsortedTransactionTest(unittest.TestCase):
+    transaction = UnsortedTransaction(
+        datetime.today().date(),
+        0.00,
+        ""
+        )
+
+    def setUp(self):
+        self.transaction = UnsortedTransaction(
+            datetime.strptime("09/20/2018", "%m/%d/%Y").date(),
+            -7.34,
+            "Alberto's"
+        )
+
+    def test_date(self):
+        self.assertEqual(self.transaction.date.month, 9)
+        self.assertEqual(self.transaction.date.day, 20)
+        self.assertEqual(self.transaction.date.year, 2018)
+
+    def test_amount(self):
+        self.assertEqual(self.transaction.amount, -7.34)
+
+    def test_location(self):
+        self.assertEqual(self.transaction.location, "Alberto's")
+
+if __name__ == "__main__":
+    unittest.main()
